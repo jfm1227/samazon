e455258759a25bf97d76e148ac6cb5f3455c9783
Akinori Ueda <aknr1130@gmail.com>
2022-02-24 22:38:56 +0900

    feature: keywordでの検索に対応

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,25 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy, :favorite]
-  PER = 15
-  
+
   def index
-    
-    
+    products = Product.all
+
     if sort_params.present?
-     @category = Category.request_category(sort_params[:sort_category])
-     @products = Product.sort_products(sort_params, params[:page])
-   elsif params[:category].present?
-     @category = Category.request_category(params[:category])
-     @products = Product.category_products(@category, params[:page])
-   else
-     @products = Product.display_list(params[:page])
-   end
-    
+      @category = Category.request_category(sort_params[:sort_category])
+      products = products.sort_products(sort_params)
+    end
+
+    if params[:category].present?
+      @category = Category.request_category(params[:category])
+      products = products.category_products(@category)
+    end
+
+    products = products.where('name LIKE ?', "%#{params[:keyword]}%") if params[:keyword].present?
+
+    @products = products.display_list(params[:page])
+
     @categories = Category.all
     @major_category_names = Category.major_categories
     @sort_list = Product.sort_list
@@ -24,24 +29,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-
-
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_url
   end
-  
+
   private
+
   def set_product
-      @product = Product.find(params[:id])
-    end
-  
+    @product = Product.find(params[:id])
+  end
+
   def product_params
-      params.require(:product).permit(:name, :description, :price, :category_id)
-    end
-  
+    params.require(:product).permit(:name, :description, :price, :category_id)
+  end
+
   def sort_params
-     params.permit(:sort, :sort_category)
-   end
+    params.permit(:sort, :sort_category)
+  end
 end
